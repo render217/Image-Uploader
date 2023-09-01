7ffd04211013d7bacb326a36576d7ec3f123d680
render217 <render21712@gmail.com>
2023-09-01 04:15:31 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ const app = express();
 
 
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 app.use('/',(req,res)=>{
     res.send('server is running')
 })
