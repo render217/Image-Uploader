7a0bc60375db4a346dbbebee5eb44a47037b2447
render217 <render21712@gmail.com>
2023-09-01 04:17:40 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 const express = require('express')
 // const connectDB = require('./src/config/database')
 const imageRoute = require('./src/api')
-const cors = require('cors')
+
 const app = express();
 
 
-app.use(cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-    });
+});
+
 app.use('/',(req,res)=>{
     res.send('server is running')
 })
