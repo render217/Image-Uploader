40fdb9801cc88ef5dde908d2d7093be9169da12a
render217 <render21712@gmail.com>
2023-09-01 12:25:35 +0300

    fixing cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://image-uploader-client-one.vercel.app"); // update to match the domain you will make the request from
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
