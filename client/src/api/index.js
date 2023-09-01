96da778a6c03dea1766ab9539177818c94bfe927
render217 <render21712@gmail.com>
2023-09-01 04:56:50 +0300

    .

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,11 +2,14 @@ const BASE_URL=`${import.meta.env.VITE_BACKEND_URL}/api/imageupload`
 
 export const uploadImage = async(formData)=>{
     
-    const response = await fetch(`${BASE_URL}`,{
+    const response = await fetch(BASE_URL,{
         method:"POST",
         headers: {
             "Content-Type": "multipart/form-data",
-        },
+            "Access-Control-Allow-Origin": "*", 
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "Content-Type",
+          },
         body:formData,
     })
     if(!response.ok){
